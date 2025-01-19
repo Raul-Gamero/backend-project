e37e07e43a28637e80d113b55566310ace903d52
Raul Gamero <raul.a.gamero@gmail.com>
2025-01-19 14:05:10 +0100

    updating the background image and adding a helper function to format the date

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,6 +2,6 @@ module.exports = {
   toUpperCase: (string) => string.toUpperCase(),
   eq: (a, b, options) => {
     console.log(options); //Debugging
-    return a === b ? options.fn(this) : options.inverse(this)
+    return a === b ? options.fn(this) : options.inverse(this);
   },
 };

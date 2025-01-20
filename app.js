274046f82caba7cf2d77a1475ca86e089cd411f5
Raul Gamero <raul.a.gamero@gmail.com>
2025-01-20 21:14:49 +0100

    updating the app.js and index.hbs files

--- app.js
+++ app.js
@@ -93,7 +93,6 @@ app.use(function (err, req, res, next) {
 
 //Render Start
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 //Render End
 
 module.exports = app;

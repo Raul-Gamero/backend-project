c7a34d0259b7899299c798fa08c332017e7a1882
Raul Gamero <raul.a.gamero@gmail.com>
2025-01-20 21:27:13 +0100

    updating the background image

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,6 @@
 module.exports = {
   toUpperCase: (string) => string.toUpperCase(),
   eq: (a, b, options) => {
-    console.log(options); //Debugging
     return a === b ? options.fn(this) : options.inverse(this);
   },
 };
